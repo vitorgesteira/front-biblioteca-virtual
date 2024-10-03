df7f644ce5d660d7b833b44faa1e209fe189d9b3
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2024-10-03 17:22:00 -0300

    criação do rodape

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./components/menu/menu.component";
+import { RodapeComponent } from "./components/rodape/rodape.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
