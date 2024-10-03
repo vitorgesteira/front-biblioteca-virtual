43dca3c7896154dd5c521302f6621d7a5ccfa44d
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2024-10-03 19:13:20 -0300

    configuração das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import {  RouterModule,Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
 
 export const routes: Routes = [];
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+// src/main.ts
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { HomeComponent } from './app/components/pages/home/home.component'; // Certifique-se de ter o HomeComponent
+import { SobreComponent } from './app/components/pages/sobre/sobre.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent},
+  // Adicione outras rotas aqui
+  { path: '**', redirectTo: '' } // Redireciona para Home em rotas não encontradas
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes)
+  ]
+})
+  .catch(err => console.error(err));
